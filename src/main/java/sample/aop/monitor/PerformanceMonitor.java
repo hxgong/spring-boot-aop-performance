cd04cb152f6bca8759cd223ba81f22f88284b5e0
hxgong <hxgong@gmail.com>
2016-01-28 16:31:52 -0600

    init import

---
+++ src/main/java/sample/aop/monitor/PerformanceMonitor.java
@@ -0,0 +1,10 @@
+package sample.aop.monitor;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PerformanceMonitor {
+
+}
