0e8fa8b85b66da62a587c067bf88dbf5139f26c7
hxgong <hxgong@gmail.com>
2016-01-28 17:12:24 -0600

    working copy on execution time.

--- src/main/java/sample/aop/monitor/PerformanceMonitor.java
+++ src/main/java/sample/aop/monitor/PerformanceMonitor.java
@@ -1,10 +1,42 @@
 package sample.aop.monitor;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
 public class PerformanceMonitor {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(PerformanceMonitor.class);
+
+	@Pointcut(value = "execution(public * *(..))")
+	public void anyPublicMethod() {
+	}
+
+	@Around("anyPublicMethod() && @annotation(ProfileExecution)")
+	public Object profileExecuteMethod(ProceedingJoinPoint jointPoint)
+			throws Throwable {
+		Signature signature = jointPoint.getSignature();
+		String methodName = signature.toShortString();
+
+		StopWatch stopWatch = new StopWatch(
+				PerformanceMonitor.class.getName());
+		stopWatch.start(methodName);
+
+		Object retVal = jointPoint.proceed();
+
+		stopWatch.stop();
+		logger.info(stopWatch.prettyPrint());
+		
+		return retVal;
+
+	}
 }
--- src/main/java/sample/aop/service/HelloWorldService.java
+++ src/main/java/sample/aop/service/HelloWorldService.java
@@ -19,12 +19,15 @@ package sample.aop.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import sample.aop.monitor.ProfileExecution;
+
 @Component
 public class HelloWorldService {
 
 	@Value("${name:World}")
 	private String name;
 
+	@ProfileExecution
 	public String getHelloMessage() {
 		return "Hello " + this.name;
 	}
